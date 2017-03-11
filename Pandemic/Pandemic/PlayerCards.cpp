93e0b7de0d52613a54835bdad2067a68e0d3f9af
marc <marc.leclair0113@gmail.com>
2017-03-11 18:45:42 -0500

    added constructor for city that includes color in form of string

--- Pandemic/Pandemic/PlayerCards.cpp
+++ Pandemic/Pandemic/PlayerCards.cpp
@@ -6,6 +6,12 @@ PlayerCard::PlayerCard() {
 	id = 0;
 	cardValue = "";
 }
+PlayerCard::PlayerCard(std::string type, int id, std::string value, std::string color) {
+	this->id = id;
+	cardValue = value;
+	this->type = type;
+	this->color = color;
+}
 PlayerCard::PlayerCard(std::string type, int id, std::string value) { // event card constructor 
 	this->id = id;
 	cardValue = value;
--- Pandemic/Pandemic/PlayerCards.h
+++ Pandemic/Pandemic/PlayerCards.h
@@ -4,11 +4,12 @@
 class PlayerCard  {
 
 
-	std::string cardValue, type;
+	std::string cardValue, type,color;
 	int id;
 
 public:
 	PlayerCard();
+	PlayerCard(std::string type, int id, std::string value, std::string color);
 	PlayerCard(std::string type, int id, std::string value);
 	std::string getValue();
 	std::string getType();
