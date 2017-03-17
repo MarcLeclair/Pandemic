ea96d55a71222e3d8366bad93f1f7011ad888b31
Ryan <rswllee@hotmail.com>
2017-03-16 21:27:58 -0400

    refactored some methods

--- Pandemic/Pandemic/Infection.h
+++ Pandemic/Pandemic/Infection.h
@@ -22,7 +22,7 @@ private:
 public:
 	Infection();
 	Infection(int); //defines Infection with an integer, being the location's ID
-	void drawInfection(Map); //draws a single infection card and adds respective amount of cubes
+	void endTurnInfection(Map); //draws a single infection card and adds respective amount of cubes
 	void shuffleInfection(); //shuffles the infection deck
 	void startInfect(Map); //beginning 9 card draws for board start state
 	void infectEpidemic(Map); //infection cards if epidemic card drawn
